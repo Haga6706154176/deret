7652a827152bde8e04b7dcd433a25d40e54acbba
sholehhermawan <irfansholehhermawan@gmail.com>
2017-04-06 09:17:23 +0700

    Add files via upload

---
+++ fibonanci.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) { 
+   int a=0,b=1,hasil=0,c;
+   Scanner input = new Scanner (System.in);
+   System.out.print ("Masukan Deret Fibonacci  : ");
+   c = input.nextInt ();
+        for(int i=1; i<=c; i++){            
+			a=b;
+			b=hasil;
+			System.out.print(hasil+" ");
+			hasil=a+b;
+		}
+} 
